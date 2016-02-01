44a9f6ffaa5c2b7a0bd36bb0e270e3df5060a039
Max Del Giudice <maxdelgiudice@gmail.com>
2016-01-31 23:02:32 -0800

    updates

--- README.md
+++ README.md
@@ -6,5 +6,65 @@ for correctness and style.
 ## Usage
 
 Place `validate_json.py` in your extension directory, and add `validate_json` to the
-`extensions` list in your conf.py file. When building your docs, add the `-b validate` flag
-to initialize JSON validation.
+`extensions` list in your conf.py file. When building your docs, add the `-b validate`
+flag to initialize JSON validation.
+
+The extension will begin looking for JSON that is either:
+
+1. Syntactically incorrect
+2. Indented inconsistently
+
+
+**Sample Output**:
+
+```bash
+sdk/bleep.rst | [INDENT ERROR] | Line 7943
+sdk/bleep.rst | [INVALID JSON] | Line 8039
+sdk/bleep.rst | [INVALID JSON] | Line 8103
+sdk/bleep.rst | [INDENT ERROR] | Line 8269
+sdk/bleep.rst | [INVALID JSON] | Line 8680
+sdk/bleep.rst | [INDENT ERROR] | Line 8680
+sdk/bleep.rst | [INVALID JSON] | Line 8807
+sdk/bleep.rst | [INVALID JSON] | Line 8890
+sdk/bleep.rst | [INDENT ERROR] | Line 8890
+sdk/other_doc.rst | [INDENT ERROR] | Line 754
+sdk/other_doc.rst | [INDENT ERROR] | Line 2374
+```
+
+
+### Manual Override
+
+Sometimes, this tool will throw an error on JSON that is technically incorrect, but still valid
+from a documentation point of view. For example, you may occasionally choose to use pseudo-json
+in your documentation, e.g.:
+
+```rst
+.. sourcecode:: json
+
+   [
+      {
+         "key1": 3,
+         "key2": "bar"
+      },
+      { "..." }
+   ]
+```
+
+In the above example, the ``{ "..." }`` just implies that there may be more objects in the list,
+but the syntax is not technically valid JSON. To prevent ``make validate`` from throwing errors
+when encountering json like this, preface the directive with ``.. json_valid``:
+
+```rst
+.. json_valid
+.. sourcecode:: json
+
+   [
+      {
+         "key1": 3,
+         "key2": "bar"
+      },
+      { "..." }
+   ]
+```
+
+The above example will no longer throw an ``[INVALID JSON]`` error.
--- validate_json.py
+++ validate_json.py
@@ -5,6 +5,14 @@ import json
 from sphinx.builders import Builder
 from sphinx.util.console import yellow, red
 
+# Various string constants
+LANG_JSON = 'json'
+LANG_HTTP = 'http'
+JSON_VALID = 'json_valid'
+JSON_OPENING_CHARS = ['[', '{']
+BLOCK_COMMENT = 'comment'
+BLOCK_LITERAL = 'literal_block'
+
 
 class JSONValidateBuilder(Builder):
     """
@@ -27,30 +35,40 @@ class JSONValidateBuilder(Builder):
 
     def write_doc(self, docname, doctree):
 
-        for node in doctree.traverse(condition=json_block):
-            lineno = node.line
+        nodes = iter(doctree.traverse())
 
-            body = node.astext()
+        for node in nodes:
 
-            # check if the json is valid.
-            if not check_json(body):
-                f = docname + '.rst'
-                msg = f + ' | [INVALID JSON] | Line ' + str(lineno)
-                self.info(red(msg))
-                self.output.write(u"%s [Invalid JSON] %s \n" % (
-                    self.env.doc2path(docname, None),
-                    lineno
-                ))
+            if valid(node):
+                try:
+                    nodes.next()
+                    nodes.next()
+                except StopIteration:
+                    break
 
-            # check if the indentation is consistent
-            if not check_indent(body):
-                f = docname + '.rst'
-                msg = f + ' | [INDENT ERROR] | Line ' + str(lineno)
-                self.info(yellow(msg))
-                self.output.write(u"%s [INDENT] %s \n" % (
-                    self.env.doc2path(docname, None),
-                    lineno
-                ))
+            if json_block(node):
+                lineno = node.line
+                body = strip_leading_chars(node.astext())
+
+                # check if the json is valid.
+                if not check_json(body):
+                    f = docname + '.rst'
+                    msg = f + ' | [INVALID JSON] | Line ' + str(lineno)
+                    self.info(red(msg))
+                    self.output.write(u"%s [Invalid JSON] %s \n" % (
+                        self.env.doc2path(docname, None),
+                        lineno
+                    ))
+
+                # check if the indentation is consistent
+                if not check_indent(body):
+                    f = docname + '.rst'
+                    msg = f + ' | [INDENT ERROR] | Line ' + str(lineno)
+                    self.info(yellow(msg))
+                    self.output.write(u"%s [INDENT] %s \n" % (
+                        self.env.doc2path(docname, None),
+                        lineno
+                    ))
 
         return
 
@@ -62,7 +80,14 @@ class JSONValidateBuilder(Builder):
 
 
 def check_json(block):
-    """ Check if the given block is valid JSON """
+    """Check if the given block is valid JSON.
+
+    Args:
+        block: A string that may or may not be valid JSON.
+
+    Returns:
+        A boolean, representing whether the block is valid JSON.
+    """
     try:
         json.loads(block)
     except ValueError, e:
@@ -111,22 +136,51 @@ def check_indent(json_string):
 
 
 def leading_wspace(string):
-    """Takes a string and returns the number of leading spaces."""
+    """Takes a string and returns the number of leading spaces.
+    """
     return len(string) - len(string.lstrip(' '))
 
 
+def strip_leading_chars(string):
+    """Takes a string containing a JSON block, and strips all leading characters
+    before the block.
+    """
+    val = 0
+    for i, c in enumerate(string):
+        if c in JSON_OPENING_CHARS:
+            val = i
+            break
+
+    return string[val:]
+
+
 def json_block(node):
-    """Check if a docutils node is a json block"""
-    if node.tagname == 'literal_block' and is_lang(node, 'json'):
+    """Check if a docutils node is a json block.
+    """
+    if node.tagname == BLOCK_LITERAL:
+        if is_lang(node, LANG_JSON):
             return True
+        if is_lang(node, LANG_HTTP):
+            txt = node.astext()
+            if any(opening_char in txt for opening_char in JSON_OPENING_CHARS):
+                return True
     return False
 
 
 def is_lang(node, language):
-    """Check the langauge of a `literal_block` node"""
+    """Check the language of a `literal_block` node.
+    """
     strnode = str(node)
-
     if 'language=\"' + language + "\"" in strnode:
+        return True
+    return False
+
+
+def valid(node):
+    """Check whether a node has been marked as valid. This allows us to mark
+    certain blocks exempt from validation.
+    """
+    if node.tagname == BLOCK_COMMENT and node.astext() == JSON_VALID:
         return True
     return False
 
